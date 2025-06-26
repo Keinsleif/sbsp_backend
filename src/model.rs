bf132c3d4d63cddca15f61845c766db8aa3fe318
Keinsleif <mountaindull@gmail.com>
2025-06-26 23:49:16 +0900

    [add] migrate model/settings.rs

--- src/model.rs
+++ src/model.rs
@@ -1,1 +1,2 @@
 mod cue;
+mod settings;
---
+++ src/model/settings.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct ShowSettings {
+    pub general: GeneralSettings,
+    // TODO Templates, Audio, Network, MIDI, OSC, Video settings
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct GeneralSettings {
+}
