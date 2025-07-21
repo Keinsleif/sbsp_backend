1d1923db19d01dace3b94cc65883133f9179443e
Keinsleif <mountaindull@gmail.com>
2025-07-21 10:50:02 +0900

    [add] ShowModel

--- src/model.rs
+++ src/model.rs
@@ -1,2 +1,15 @@
-mod cue;
+use serde::{Deserialize, Serialize};
+
+use crate::model::{cue::Cue, settings::ShowSettings};
+
+pub mod cue;
 mod settings;
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct ShowModel {
+    pub name: String,
+    pub cues: Vec<Cue>,
+    pub settings: ShowSettings,
+}
